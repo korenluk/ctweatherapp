68b2801ab903ba4dbcdfbca785115ac617010b2f
Lukas Korinek <lukas.korinek@eman.cz>
2025-08-19 14:17:50 +0200

    feat: add implementation of WeatherDataProvider

--- CTWeatherApp/DataProviders/WeatherDataProvider.swift
+++ CTWeatherApp/DataProviders/WeatherDataProvider.swift
@@ -5,12 +5,15 @@
 //  Created by David Pařízek on 22.07.2025.
 //
 
-import Combine
 import Foundation
 
-//Tato třída bude držet a načítat data z API
 
-@MainActor
-final class WeatherDataProvider: ObservableObject {
-
+final class WeatherDataProvider {
+    private let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max&daily=temperature_2m_min")!
+    
+    private let client = RestApiClient()
+    
+    func getData() async throws -> WeatherResponse {
+        try await client.request(url: url)
+    }
 }
